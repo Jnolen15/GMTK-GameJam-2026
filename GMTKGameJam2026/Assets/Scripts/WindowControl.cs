12a1e4b6e2ade36d7b41a222e7d03d3655babbc7
Jared Nolen <legodoghead@gmail.com>
2026-07-23 22:13:57 -0700

    Draggable Windows
    - Two new scripts that allow for windows to be dragged around the desktop

--- GMTKGameJam2026/Assets/Scripts/WindowControl.cs
+++ GMTKGameJam2026/Assets/Scripts/WindowControl.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class WindowControl : MonoBehaviour
+public class WindowControl : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // =================== Refrences ===================
+    private Canvas _canvas;
+    private Camera _cam;
+    private RectTransform _rectTransform;
+    private WindowManager _winMan;
+    private bool _moveable;
+
+    // =================== Setup ===================
+    #region Setup
+    private void Start()
+    {
+        _canvas = GetComponentInParent<Canvas>();
+        _cam = _canvas.worldCamera;
+        _rectTransform = GetComponent<RectTransform>();
+        _winMan = GetComponentInParent<WindowManager>();
+    }
+    #endregion
+
+    // =================== Function ===================
+    #region Function
+    private void Update()
     {
         
+    }
+    #endregion
+
+    // =================== Interface ===================
+    #region Interface
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(_moveable)
+            _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_rectTransform, eventData.position, _cam, out Vector2 localPoint))
+        {
+            _winMan.MakeMeFavoriteChild(_rectTransform);
+
+            float halfHeight = (_rectTransform.rect.height / 2);
+            float topZone = (halfHeight - 50);
+            Debug.Log($"Mouse Position inside UI: {localPoint} {halfHeight} {topZone}");
+
+            if (localPoint.y > topZone)
+                _moveable = true;
+            else
+                _moveable = false;
+        }
     }
+    #endregion
 }
