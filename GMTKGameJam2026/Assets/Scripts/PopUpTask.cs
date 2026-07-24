1bb4deb08299b11f02974b8cafb4bfc8ef773324
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2026-07-23 18:14:01 -0700

    created inheritence template

---
+++ GMTKGameJam2026/Assets/Scripts/PopUpTask.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PopUpTask : Task
+{
+
+    public PopUpTask(string input) : base(input)
+    {
+
+    }
+    
+    void Start()
+    {
+        // subscribe to stuff
+    }
+
+    private void OnDestroy()
+    {
+       // unsubscribe from stuff
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
